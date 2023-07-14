38329a3d488209cbbab5171567ada1abc73abe94
Enrico Emil Dela Rosa <enricodelarosa01@gmail.com>
2023-07-15 00:28:51 +0800

    Address QA feedback on shop, collection, and product pages

--- src/pages/collection/Breadcrumbs/Breadcrumbs.js
+++ src/pages/collection/Breadcrumbs/Breadcrumbs.js
@@ -6,9 +6,11 @@ function Breadcrumbs() {
                 <div className='row breadcrumbs__list'>
                     <div className='col-12'>
                         <ul>
-                            <li><a className="breadcrumbs__list-main" href="">Home</a></li>
+                        <li><a className="breadcrumbs__list-main" href="">Home</a></li>
                             <li><img src='../assets/layouts/collection/breadcrumbs-seperator.svg' alt="Breadcrumb Icon Seperator"/></li>
-                            <li><a className="breadcrumbs__list-current" href="">Afloat collection</a></li>
+                            <li><a className="breadcrumbs__list-current" href="">Silver collection</a></li>
+                            <li><img src='../assets/layouts/collection/breadcrumbs-seperator.svg' alt="Breadcrumb Icon Seperator"/></li>
+                            <li><a className="breadcrumbs__list-category" href="">A Box of Dallas</a></li>
                         </ul>
                     </div>
                 </div>
--- src/pages/shop/Filter/Filter.js
+++ src/pages/shop/Filter/Filter.js
@@ -1,16 +1,24 @@
 import './filter.css'
 import { useState, useRef } from 'react';
-import { FILTER_SORT, FILTER_PRODUCT } from './constants';
+import { FILTER_SORT, FILTER_PRODUCT, FILTER_CONSTANTS, FILTER_CARDS } from './constants';
 import { Link } from 'react-router-dom'
 
 function Filter() {
-    // const store = Object.keys(FILTER_SORT)[2] || '';
-    // const storeSpace = store.replace(/([A-Z])/g, ' $1').trim();
+    const store = Object.keys(FILTER_CONSTANTS)[2] || '';
+    const storeSpace = store.replace(/([A-Z])/g, ' $1').trim();
+
+
 
     const [SortClicked, setSortClicked] = useState(false);
+    const [LocationClicked, setLocationClicked] = useState(false);
+    const [StoretypeClicked, setStoretypeClicked] = useState(false);
     const [OccasionClicked, setOccasionClicked] = useState(false);
 
     const ref = useRef(null);
+
+    const triggerLocationClicked = () => {
+        setLocationClicked(!LocationClicked)
+    }
 
     const triggerSortClicked = () => {
         setSortClicked(!SortClicked)
@@ -20,13 +28,17 @@ function Filter() {
         setOccasionClicked(!OccasionClicked)
     }
 
+    const triggerStoretypeClicked = () => {
+        setStoretypeClicked(!StoretypeClicked)
+    }
+
     return (
         <div className='shop__filter'>
             <div className='container'>
                 <div className='row shop__filter-col justify-content-between'>
                     <div className='col-3'>
                         <div className='sidebar'>
-                            <div className='sidebar__location'>
+                            <div style={{marginBottom: '80px'}} className='sidebar__location'>
                                 <span className='sidebar__label'>{Object.keys(FILTER_SORT)[0]}</span>
                                 <span className={`sidebar__label-arrow ` + (SortClicked ? '' : '')} onClick={() => triggerSortClicked()}><img src='/assets/layouts/chevron-up.svg' alt="up arrow" /></span>
                                 <div ref={ref} className={`sidebar__list-mobile ` + (SortClicked ? 'show-list' : 'hide-list')}>
@@ -44,6 +56,26 @@ function Filter() {
                                 </ul>
 
                             </div>
+
+                            <div className='sidebar__location'>
+                                <span className='sidebar__label'>{Object.keys(FILTER_CONSTANTS)[0]}</span>
+                                <span className={`sidebar__label-arrow ` + (LocationClicked ? '' : '')} onClick={() => triggerLocationClicked()}><img src='/assets/layouts/chevron-up.svg' alt="up arrow" /></span>
+                                <div ref={ref} className={`sidebar__list-mobile ` + (LocationClicked ? 'show-list' : 'hide-list')}>
+                                    <ul className='sidebar__list'>
+                                        {FILTER_CONSTANTS.location.map((loc) => (
+                                            <li><label><input type="radio" />{loc}</label></li>
+                                        ))}
+                                    </ul>
+                                </div>
+
+                                <ul className='sidebar__list-desktop'>
+                                    {FILTER_CARDS && FILTER_CONSTANTS.location.map((loc) => (
+                                        <li><label><input type="radio" />{loc}</label></li>
+                                    ))}
+                                </ul>
+
+                            </div>
+
                             <div className='sidebar__occasion'>
                                 <span className='sidebar__label'>{Object.keys(FILTER_SORT)[1]}</span>
                                 <span className={`sidebar__label-arrow ` + (OccasionClicked ? '' : '')} onClick={() => triggerOccasionClicked()}><img src='/assets/layouts/chevron-up.svg' alt="arrow up" /></span>
@@ -62,7 +94,26 @@ function Filter() {
                                 </ul>
                             </div>
 
+                            <div className='sidebar__storetype'>
+                                <span className='sidebar__label'>{storeSpace}</span>
+                                <span className={`sidebar__label-arrow ` + (StoretypeClicked ? '' : '')} onClick={() => triggerStoretypeClicked()}><img src='/assets/layouts/chevron-up.svg' alt="arrow up" /></span>
+                                <div ref={ref} className={`sidebar__list-mobile ` + (StoretypeClicked ? 'show-list' : 'hide-list')}>
+                                    <ul className='sidebar__list'>
+                                        {FILTER_CONSTANTS && FILTER_CONSTANTS.storeType.map((store) => (
+                                            <li><label><input type="checkbox" />{store}</label></li>
+                                        ))}
+                                    </ul>
+                                </div>
+
+                                <ul className='sidebar__list-desktop'>
+                                    {FILTER_CONSTANTS && FILTER_CONSTANTS.storeType.map((store) => (
+                                        <li><label><input type="checkbox" />{store}</label></li>
+                                    ))}
+                                </ul>
+                            </div>
+
                         </div>
+                        
                     </div>
                     <div className='col-9'>
                         <div className='shop__filter-result'>
