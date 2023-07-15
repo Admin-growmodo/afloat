362432ca8ed3a02cbe573b4e2b21ee0b9a87f158
Admin-growmodo <enricodelarosa01@gmail.com>
2023-07-15 20:05:04 +0800

    Fix header spacing and breacrumbs linking

--- src/pages/collection/Breadcrumbs/Breadcrumbs.js
+++ src/pages/collection/Breadcrumbs/Breadcrumbs.js
@@ -1,16 +1,19 @@
 import './breadcrumbs.css'
+import {useNavigate} from 'react-router-dom';
+
 function Breadcrumbs() {
+    const navigate = useNavigate();
     return (
         <div className='breadcrumbs'>
             <div className='container'>
                 <div className='row breadcrumbs__list'>
                     <div className='col-12'>
                         <ul>
-                        <li><a className="breadcrumbs__list-main" href="">Home</a></li>
+                            <li><a onClick={e => {navigate('/homepage')}} className="breadcrumbs__list-main" href="">Home</a></li>
                             <li><img src='../assets/layouts/collection/breadcrumbs-seperator.svg' alt="Breadcrumb Icon Seperator"/></li>
-                            <li><a className="breadcrumbs__list-current" href="">Silver collection</a></li>
+                            <li><a className="breadcrumbs__list-current" href="">Afloat collection</a></li>
                             <li><img src='../assets/layouts/collection/breadcrumbs-seperator.svg' alt="Breadcrumb Icon Seperator"/></li>
-                            <li><a className="breadcrumbs__list-category" href="">A Box of Dallas</a></li>
+                            <li><a onClick={e => {navigate('/shop')}} className="breadcrumbs__list-category" href="">A Box of Dallas</a></li>
                         </ul>
                     </div>
                 </div>
