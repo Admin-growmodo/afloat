d93065f6ba27dd34b97bef4805cb15ba262a816d
Admin-growmodo <enricodelarosa01@gmail.com>
2023-07-15 20:12:43 +0800

    Remove filter for location and store type

--- src/pages/shop/Filter/Filter.js
+++ src/pages/shop/Filter/Filter.js
@@ -59,25 +59,6 @@ function Filter() {
 
                             </div>
 
-                            <div className='sidebar__location'>
-                                <span className='sidebar__label'>{Object.keys(FILTER_CONSTANTS)[0]}</span>
-                                <span className={`sidebar__label-arrow ` + (LocationClicked ? '' : '')} onClick={() => triggerLocationClicked()}><img src='/assets/layouts/chevron-up.svg' alt="up arrow" /></span>
-                                <div ref={ref} className={`sidebar__list-mobile ` + (LocationClicked ? 'show-list' : 'hide-list')}>
-                                    <ul className='sidebar__list'>
-                                        {FILTER_CONSTANTS.location.map((loc) => (
-                                            <li><label><input onClick={e => {setShowEmpty(!showEmpty)}} type="radio" name="location-mobile"/>{loc}</label></li>
-                                        ))}
-                                    </ul>
-                                </div>
-
-                                <ul className='sidebar__list-desktop'>
-                                    {FILTER_CARDS && FILTER_CONSTANTS.location.map((loc) => (
-                                        <li><label><input onClick={e => {setShowEmpty(!showEmpty)}} type="radio" name="location"/>{loc}</label></li>
-                                    ))}
-                                </ul>
-
-                            </div>
-
                             <div className='sidebar__occasion'>
                                 <span className='sidebar__label'>{Object.keys(FILTER_SORT)[1]}</span>
                                 <span className={`sidebar__label-arrow ` + (OccasionClicked ? '' : '')} onClick={() => triggerOccasionClicked()}><img src='/assets/layouts/chevron-up.svg' alt="arrow up" /></span>
@@ -95,25 +76,6 @@ function Filter() {
                                     ))}
                                 </ul>
                             </div>
-
-                            <div className='sidebar__storetype'>
-                                <span className='sidebar__label'>{storeSpace}</span>
-                                <span className={`sidebar__label-arrow ` + (StoretypeClicked ? '' : '')} onClick={() => triggerStoretypeClicked()}><img src='/assets/layouts/chevron-up.svg' alt="arrow up" /></span>
-                                <div ref={ref} className={`sidebar__list-mobile ` + (StoretypeClicked ? 'show-list' : 'hide-list')}>
-                                    <ul className='sidebar__list'>
-                                        {FILTER_CONSTANTS && FILTER_CONSTANTS.storeType.map((store) => (
-                                            <li><label><input type="checkbox" />{store}</label></li>
-                                        ))}
-                                    </ul>
-                                </div>
-
-                                <ul className='sidebar__list-desktop'>
-                                    {FILTER_CONSTANTS && FILTER_CONSTANTS.storeType.map((store) => (
-                                        <li><label><input type="checkbox" />{store}</label></li>
-                                    ))}
-                                </ul>
-                            </div>
-
                         </div>
                         
                     </div>
